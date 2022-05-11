0a847b1186ca494b36de24257ca0b00c3e46a279
0X7FFFFFFF <31471833+Int-0X7FFFFFFF@users.noreply.github.com>
2022-05-11 16:52:38 -0600

    petpet fix

---
+++ modules/Ban_me_random.py
@@ -0,0 +1,28 @@
+import random
+
+from graia.ariadne.app import Ariadne
+from graia.ariadne.event.message import GroupMessage
+from graia.ariadne.message.chain import MessageChain
+from graia.ariadne.message.element import *
+from graia.ariadne.message.parser.twilight import Twilight, RegexMatch
+from graia.ariadne.model import Group, Member
+from graia.saya import Channel
+from graia.saya.builtins.broadcast.schema import ListenerSchema
+
+channel = Channel.current()
+
+channel.name("BanMe")
+channel.description("发送'禁言我'禁言(前提是有权限)")
+channel.author("INT_MAX")
+
+@channel.use(ListenerSchema(
+    listening_events=[GroupMessage],
+    inline_dispatchers=[Twilight([RegexMatch(".?禁言我.?")])]
+))
+async def auto_ban(app: Ariadne, group: Group, member: Member):
+    Ban_time_random = await random.Random()
+    try:
+        await app.muteMember(group, member, Ban_time_random)
+        await app.sendGroupMessage(group, MessageChain.create('那我就来实现你的愿望吧！'))
+    except PermissionError:
+        await app.sendGroupMessage(group, MessageChain.create('对不起，我没有办法实现你的愿望555~'))
---
+++ modules/support_of_me/support.py
@@ -0,0 +1,69 @@
+import asyncio
+from io import BytesIO
+from pathlib import Path
+
+import aiohttp
+import imageio
+from graia.ariadne.app import Ariadne
+from graia.ariadne.event.message import GroupMessage
+from graia.ariadne.message.chain import MessageChain
+from graia.ariadne.message.element import *
+from graia.ariadne.message.parser.twilight import (FullMatch, MatchResult,
+                                                   Twilight, WildcardMatch)
+from graia.ariadne.model import Group, Member
+from graia.saya import Channel
+from graia.saya.builtins.broadcast.schema import ListenerSchema
+from PIL import Image as IMG
+
+channel = Channel.current()
+
+channel.name("petpet")
+channel.description("发送'摸头@某人'制作摸头GIF")
+channel.author("IMTMAX")
+
+import asyncio
+from io import BytesIO
+from pathlib import Path
+
+import aiohttp
+import imageio
+from graia.ariadne.app import Ariadne
+from graia.ariadne.event.message import GroupMessage
+from graia.ariadne.message.chain import MessageChain
+from graia.ariadne.message.element import *
+from graia.ariadne.message.parser.twilight import (FullMatch, MatchResult,
+                                                   Twilight, WildcardMatch)
+from graia.ariadne.model import Group, Member
+from graia.saya import Channel
+from graia.saya.builtins.broadcast.schema import ListenerSchema
+from PIL import Image as IMG
+
+channel = Channel.current()
+
+channel.name("support")
+channel.description("发送'精神支柱@某人'制作摸头GIF")
+channel.author("IMTMAX")
+
+
+async def support(file, squish=0):
+    avatar = IMG.open(file).convert("RGBA")
+    support = IMG.open(Path(__file__).parent/'support.png')
+    frame = IMG.new('RGBA', (1293, 1164), (255, 255, 255, 0))
+    avatar = avatar.resize((815, 815), IMG.ANTIALIAS).rotate(23, expand=True)
+    frame.paste(avatar, (-172, -17))
+    frame.paste(support, mask=support)
+    frame = frame.convert('RGB')
+    frame.save(ret := BytesIO(), format='jpeg')
+    return ret
+
+@channel.use(ListenerSchema(
+    listening_events=[GroupMessage],
+    inline_dispatchers=[Twilight([FullMatch("精神支柱"), WildcardMatch() @ "para"])]
+))
+async def petpet(app: Ariadne, group: Group, member: Member, para: MatchResult):
+    user = para.result.getFirst(At).target if para.matched and para.result.has(At) else member.id
+    profile_url = f"http://q1.qlogo.cn/g?b=qq&nk={user}&s=640"
+    async with aiohttp.request("GET", profile_url) as r:
+        profile = BytesIO(await r.read())
+    pic = await support(profile)
+    await app.sendGroupMessage(group, MessageChain.create([Image(data_bytes=pic.getvalue())]))
