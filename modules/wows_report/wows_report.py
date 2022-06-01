3d8228b2e28ca9bbcf86712e9f35ba66b1470e84
0X7FFFFFFF <31471833+Int-0X7FFFFFFF@users.noreply.github.com>
2022-05-31 21:54:07 -0600

    ocr在线识别

--- modules/wows_report/wows_report.py
+++ modules/wows_report/wows_report.py
@@ -1,3 +1,7 @@
+import base64
+
+from graia.ariadne import get_running
+from graia.ariadne.adapter import Adapter
 from graia.ariadne.app import Ariadne
 from graia.ariadne.event.message import GroupMessage, MessageEvent
 from graia.ariadne.message.chain import MessageChain
@@ -9,9 +13,15 @@ from io import BytesIO
 from graia.saya import Channel
 from graia.saya.builtins.broadcast.schema import ListenerSchema
 from PIL import Image as IMG, ImageFilter, ImageDraw
-import easyocr
 
+offline = False
+if offline:
+    import easyocr
 channel = Channel.current()
+
+APIKEY = '1145141919810'
+
+API = "https://api.ocr.space/parse/imageurl?apikey=APIKEY&url=PIC_URL&language=chs&OCREngine=3&scale=true"
 
 
 def pic_cut(file):
@@ -43,26 +53,51 @@ def ocr_read(file):
 
 @channel.use(ListenerSchema(
     listening_events=[GroupMessage],
-    inline_dispatchers=[Twilight([WildcardMatch() @ "para", FullMatch("举报"), ])]
+    inline_dispatchers=[Twilight([WildcardMatch() @ "para", FullMatch("口"), ])]
 ))
 async def report(app: Ariadne, group: Group, message: MessageChain, para: MatchResult):
-    # await app.sendMessage(group, MessageChain.create('OCR检测开始'))
-    target = para.result.getFirst(At).target
-    org_element: Quote = message[1]
-    message_id = org_element.id
-    event: MessageEvent = await app.getMessageFromId(message_id)
-    msg_chain = event.messageChain
-    org_img = msg_chain.get(Image)[0]
-    input = BytesIO(await org_img.get_bytes())
-    out = pic_cut(input)
-    res = ocr_read(file=input)
-    dic_ship = get_ship_data()
-    for res_sig in res:
-        if res_sig in dic_ship.keys():
-            await app.sendMessage(group,
-                                  MessageChain.create(f'检测到船只 {res_sig}'))
-            try:
-                await app.muteMember(group, target, 1)
-            except PermissionError:
-                await app.sendGroupMessage(group, MessageChain.create('ERROR:权限不足'))
-            break
+    if offline:
+        # await app.sendMessage(group, MessageChain.create('OCR检测开始'))
+        target = para.result.getFirst(At).target
+        org_element: Quote = message[1]
+        message_id = org_element.id
+        event: MessageEvent = await app.getMessageFromId(message_id)
+        msg_chain = event.messageChain
+        org_img = msg_chain.get(Image)[0]
+        input = BytesIO(await org_img.get_bytes())
+        out = pic_cut(input)
+        res = ocr_read(file=input)
+        dic_ship = get_ship_data()
+        for res_sig in res:
+            if res_sig in dic_ship.keys():
+                await app.sendMessage(group,
+                                      MessageChain.create(f'检测到船只 {res_sig}'))
+                try:
+                    await app.muteMember(group, target, 3600)
+                except PermissionError:
+                    await app.sendGroupMessage(group, MessageChain.create('ERROR:权限不足'))
+                break
+    else:
+        target = para.result.getFirst(At).target
+        org_element: Quote = message[1]
+        message_id = org_element.id
+        event: MessageEvent = await app.getMessageFromId(message_id)
+        msg_chain = event.messageChain
+        org_img = msg_chain.get(Image)[0]
+        input = BytesIO(await org_img.get_bytes())
+        out = pic_cut(input)
+        url: str = org_img.url
+        session = get_running(Adapter).session
+        async with session.get(API.replace('APIKEY', APIKEY).replace('PIC_URL', url)) as resp:  # type: ignore
+            data = await resp.json()
+            dic = get_ship_data()
+            val = data['ParsedResults'][0]['ParsedText']
+            for ship in dic.keys():
+                if ship in val:
+                    await app.sendMessage(group,
+                                          MessageChain.create(f'检测到船只 {ship}'))
+                    try:
+                        await app.muteMember(group, target, 3600)
+                    except PermissionError:
+                        await app.sendGroupMessage(group, MessageChain.create('ERROR:权限不足'))
+                    break
